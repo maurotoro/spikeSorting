9151225f650cb14d94923f01c4376490a0fd49cb
Mauricio Toro <mauro.toro.e@gmail.com>
2017-05-04 00:07:50 +0100

    update

--- Poirazi.py
+++ Poirazi.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 29 14:52:59 2016
+Created on Wed Jun 29 14:52:59 2016.
 
 Crude implementation of the proposal by:
-    Panagiotis C. Petrantonakis and Panayiota Poirazi, 2015 
+    Panagiotis C. Petrantonakis and Panayiota Poirazi, 2015
 For this to work you'll need to  have installed all necesary python
 libraries, look at the import statements and change the paths in the functions
 
@@ -20,11 +20,13 @@ import scipy.io as sio
 
 
 def Dw(sig, w=5, s=1, norm=0):
-    '''
-        Goes through the signal summing the euclidean distance of each
-        w points window in s steps. Making each signal more informative
-        about the underlying content.
-    '''
+    """
+    Create a new signal by calculating euclidean distance of moving windows.
+
+    Go through the signal summing the euclidean distance of each w points
+    window in s steps. Making each signal more informative about the
+    underlying content.
+    """
     if norm == 1:
         sig = np.array((sig-np.mean(sig))/np.std(sig))
     f = np.array([sig[i*s:(i*s)+w] for i in range(int((len(sig)-w)/s)+1)])
@@ -46,8 +48,9 @@ def cart3d2sphe(x, y, z):
 
 
 def spkDet(signal, method='QQ', T=3, ori='neg', lim=10):
-    '''
+    """
     Detect events on signal depending on method. Can be:
+
     MAD:
         Median Absolute Deviation:
             median(absolute(signal - median(signal)))
@@ -56,7 +59,7 @@ def spkDet(signal, method='QQ', T=3, ori='neg', lim=10):
             median(|signal|/.6745)
     Then uses that method times T to detect depending on orientation ori
     Detect crossings and keep only min point
-    '''
+    """
     if method == 'QQ':
         QQ = np.median(np.abs(signal)/0.6745)
         thresh = QQ*T  # High THRSH
@@ -76,10 +79,11 @@ def spkDet(signal, method='QQ', T=3, ori='neg', lim=10):
 
 
 def spkAlign(spX, signal, d=10, D=[12, 20], method='MIN'):
-    '''
-    Align detected spikes and return the waveform from D[0] to D[1]
-    MIN: Use the argmin from d+\- spX
-    '''
+    """
+    Align detected spikes and return the waveform from D[0] to D[1].
+
+    MIN: Use the argmin from d+/- spX
+    """
     spkIX = [None]*len(spX)
     if method == 'MIN':
         for i in range(len(spX)):
@@ -90,30 +94,31 @@ def spkAlign(spX, signal, d=10, D=[12, 20], method='MIN'):
 
 
 def loadAK(filename, numChannels, dtype):
-    '''
-    Load electrodes data into memory, filename should be
-    fpa+fpb+ele
+    """
+    Load electrodes data into memory, filename should be fpa+fpb+ele.
+
     Where:
         fpa = '/Users/soyunkope/Documents/scriptkidd/git/spikeSorting/'
         For test with Kampf's:
             fpb = 'data/kampfLab/'
-            ele = 'adc2014-11-25T23_00_08.bin' 
+            ele = 'adc2014-11-25T23_00_08.bin'
 
     Filename:  String that points to the raw data file
     numChannels:  Number of channels on the probe
     dtype:  DType of the record
-    '''
-    fdata = np.fromfile(filename,dtype=dtype)
+    """
+    fdata = np.fromfile(filename, dtype=dtype)
     numsamples = int(len(fdata) / numChannels)
-    data = np.reshape(fdata,(numsamples,numChannels))
+    data = np.reshape(fdata, (numsamples, numChannels))
     return (np.transpose(data))
 
 def loadJP(uarray, electrode, dur):
-    '''
+    """
     For test with Joe's electrodes
+
         uarray:  str(Array) ['F32', 'L32']
         GT02 = '/Users/soyunkope/Documents/INDP2015/2016_S02/R01_JoePaton/00_AutoSpikeSorting/TestDAta/GT02/GroundTruth6ch.dat'
-    '''
+    """
     fpa = '/Users/soyunkope/Documents/scriptkidd/git/spikeSorting/'
     fpb = 'data/patonLab/'
     fur = str(uarray)+'/'+str(electrode)+'/'
@@ -126,18 +131,21 @@ def loadJP(uarray, electrode, dur):
 def loadQQ(N):
     fPA = '/Users/soyunkope'
     fPB = '/Documents/DataSets/Ephys/Simulator/'
-    fSig = ['C_Burst_Easy2_noise015.mat', 'C_Difficult1_noise005.mat',
-            'C_Difficult1_noise01.mat', 'C_Difficult1_noise015.mat',
-            'C_Difficult1_noise02.mat', 'C_Difficult2_noise005.mat',
-            'C_Difficult2_noise01.mat', 'C_Difficult2_noise015.mat',
-            'C_Difficult2_noise02.mat', 'C_Drift_Easy2_noise015.mat',
-            'C_Easy1_noise005.mat', 'C_Easy1_noise01.mat', 'C_Easy1_noise015.mat',
-            'C_Easy1_noise01_short.mat', 'C_Easy1_noise02.mat',
-            'C_Easy1_noise025.mat', 'C_Easy1_noise03.mat',
-            'C_Easy1_noise035.mat', 'C_Easy1_noise04.mat',
+    fSig = ['C_Burst_Easy2_noise015.mat',
+            'C_Difficult1_noise005.mat', 'C_Difficult1_noise01.mat',
+            'C_Difficult1_noise015.mat', 'C_Difficult1_noise02.mat',
+            'C_Difficult2_noise005.mat', 'C_Difficult2_noise01.mat',
+            'C_Difficult2_noise015.mat', 'C_Difficult2_noise02.mat',
+            'C_Drift_Easy2_noise015.mat',
+            'C_Easy1_noise005.mat', 'C_Easy1_noise01.mat',
+            'C_Easy1_noise015.mat', 'C_Easy1_noise01_short.mat',
+            'C_Easy1_noise02.mat', 'C_Easy1_noise025.mat',
+            'C_Easy1_noise03.mat', 'C_Easy1_noise035.mat',
+            'C_Easy1_noise04.mat',
             'C_Easy2_noise005.mat', 'C_Easy2_noise01.mat',
             'C_Easy2_noise015.mat', 'C_Easy2_noise02.mat',
-            'C_Test_LFPcorr_Easy2_noise015.mat',  'times_C_Difficult1_noise015.mat'
+            'C_Test_LFPcorr_Easy2_noise015.mat',
+            'times_C_Difficult1_noise015.mat'
             ]
     fname = fPA+fPB+fSig[N]
     fdata = sio.loadmat(fname, struct_as_record=0, squeeze_me=1)
@@ -146,10 +154,27 @@ def loadQQ(N):
 
 
 def main(N):
-    '''
-        Makes this work for you by changing the paths to your need,
-        Read the functions, some of them are sort of documented
-    '''
+    """
+    Make this work for you by changing the paths to your need,
+
+    Read the functions, some of them are sort of documented
+    """
+    fSig = ['C_Burst_Easy2_noise015.mat',
+            'C_Difficult1_noise005.mat', 'C_Difficult1_noise01.mat',
+            'C_Difficult1_noise015.mat', 'C_Difficult1_noise02.mat',
+            'C_Difficult2_noise005.mat', 'C_Difficult2_noise01.mat',
+            'C_Difficult2_noise015.mat', 'C_Difficult2_noise02.mat',
+            'C_Drift_Easy2_noise015.mat',
+            'C_Easy1_noise005.mat', 'C_Easy1_noise01.mat',
+            'C_Easy1_noise015.mat', 'C_Easy1_noise01_short.mat',
+            'C_Easy1_noise02.mat', 'C_Easy1_noise025.mat',
+            'C_Easy1_noise03.mat', 'C_Easy1_noise035.mat',
+            'C_Easy1_noise04.mat',
+            'C_Easy2_noise005.mat', 'C_Easy2_noise01.mat',
+            'C_Easy2_noise015.mat', 'C_Easy2_noise02.mat',
+            'C_Test_LFPcorr_Easy2_noise015.mat',
+            'times_C_Difficult1_noise015.mat'
+            ]
     sig = loadQQ(N)
     dF = Dw(sig, w=5, s=1)
     dQ = Dw(sig, w=15, s=1)
@@ -178,13 +203,15 @@ def main(N):
         axb[i].plot(wav[0]+wav[1], 'k--', alpha=.25)
         axb[i].plot(wav[0]-wav[1], 'k--', alpha=.25)
         [axb[i].plot(waves[j], cols[i], alpha=.15) for j in ndx[samp]]
-    [ax[j].set_xticks([]) for j in [0, 1]]
-    [ax[j].set_yticks([]) for j in [0, 1]]
-    [axb[j].set_ylim([-2, 2]) for j in range(4)]
-    [axb[j].set_xlim([-1, 25]) for j in range(4)]
-    [axb[j].set_xticks([]) for j in range(4)]
-    [axb[j].set_yticks([]) for j in range(4)]
+        [ax[j].set_xticks([]) for j in [0, 1]]
+        [ax[j].set_yticks([]) for j in [0, 1]]
+        [axb[j].set_ylim([-2, 2]) for j in range(4)]
+        [axb[j].set_xlim([-1, 25]) for j in range(4)]
+        [axb[j].set_xticks([]) for j in range(4)]
+        [axb[j].set_yticks([]) for j in range(4)]
     fa.suptitle(fSig[N], va='top', ha='center', x=.5, y=.95)
     plt.tight_layout()
 
-main(5)
+
+if __name__() == "__main__":
+    main(5)
